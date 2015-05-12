4f0b5a25f37b1ca1f4c7bd24bd0a04ef23a7a48c
NguaGo <nguago@let.vn>
2015-05-12 15:17:09 +0700

    Use tinymce/tinymce composer

--- TinymceAssets.php
+++ TinymceAssets.php
@@ -4,12 +4,11 @@ namespace letyii\tinymce;
 
 class TinymceAssets extends \yii\web\AssetBundle
 {
-	public $sourcePath = '@vendor/letyii/yii2-tinymce';
+	public $sourcePath = '@vendor/tinymce/tinymce';
 	public $js = [
-		'tinymce/tinymce.min.js',
+		'tinymce.min.js',
 	];
 	public $depends = [
-		'yii\web\YiiAsset',
 		'yii\web\JqueryAsset',
 	];
 }
